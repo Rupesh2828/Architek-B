5744af27b47512a43253c2a5f0c9f3de6e7d3c4f
Rupesh Jadhav <rupesh.jadhav7700@gmail.com>
2025-03-24 05:27:49 +0530

    start project

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ async function server() {
     try {
         await prisma.$connect();
         console.log("Connected to Postgres!");
+        
 
         app.listen(PORT, () => {
             console.log(`Server is running on PORT: ${PORT}`);
