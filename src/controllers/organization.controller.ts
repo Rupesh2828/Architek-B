3aec2a8c56c20032615f7cbb03b5d83838ebc02b
Rupesh Jadhav <rupesh.jadhav7700@gmail.com>
2025-04-18 17:03:32 +0530

    implement organization creation and deletion endpoints with error handling

--- src/controllers/organization.controller.ts
+++ src/controllers/organization.controller.ts
@@ -2,36 +2,76 @@ import { Request, Response } from "express";
 import prisma from "../config/database";
 
 export const createOrganizationWithOwner = async (req: Request, res: Response): Promise<void> => {
-    try {
-      const { name } = req.body;
-      const userId = req.session.userId; 
-  
-      if (!name || !userId) {
-        res.status(400).json({ message: "Organization name and user ID are required." });
-        return;
-      }
-  
-      await prisma.$transaction(async (tx) => {
-        const org = await tx.organization.create({
-          data: {
-            name,
-          },
-        });
-  
-        await tx.organizationMember.create({
-          data: {
-            userId,
-            organizationId: org.id,
-            role: "OWNER",
-          },
-        });
-      });
-  
-      res.status(201).json({ message: "Organization created with owner" });
-  
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal Server Error" });
+  try {
+    const { name } = req.body;
+    const userId = req.session.userId;
+
+    if (!name || !userId) {
+      res.status(400).json({ message: "Organization name and user ID are required." });
+      return;
     }
-  };
+
+    await prisma.$transaction(async (tx) => {
+      const org = await tx.organization.create({
+        data: {
+          name,
+        },
+      });
+
+      await tx.organizationMember.create({
+        data: {
+          userId,
+          organizationId: org.id,
+          role: "OWNER",
+        },
+      });
+    });
+
+    res.status(201).json({ message: "Organization created with owner" });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const deleteOrganizationCascade = async (req: Request, res: Response): Promise<void> => {
+
+  try {
+    const { id } = req.params;
   
+    if (!id) {
+      res.status(400).json({ message: "OrgId is required" })
+  
+    }
+  
+    const deleteOrgMember = prisma.organizationMember.deleteMany({
+      where: {
+        organizationId: id,
+      },
+    })
+  
+    const deleteProject = prisma.project.deleteMany({
+      where: {
+        organizationId: id,
+      },
+    })
+  
+    const deleteOrg = prisma.organization.delete({
+      where: {
+        id: id,
+      },
+    })
+  
+    await prisma.$transaction([deleteOrgMember, deleteProject, deleteOrg]) 
+
+    res.status(201).json({ message: "Organization deleted successfully" });
+  
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+    
+  }
+
+
+}
--- src/routes/organization.route.ts
+++ src/routes/organization.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createOrganizationWithOwner } from "../controllers/organization.controller";
+import { createOrganizationWithOwner, deleteOrganizationCascade } from "../controllers/organization.controller";
 
 const router = Router()
 
 router.route('/create').post(createOrganizationWithOwner)
+router.route('/delete/:id').delete(deleteOrganizationCascade)
 
 
 export default router;
